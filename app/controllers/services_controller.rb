f923bd904bf256df94b4c0c171cfc917047842e0
Tom Scott <tscott@telvue.com>
2014-06-14 11:44:57 -0400

    Add services API

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,44 @@
 class ServicesController < ApplicationController
+  respond_to :json
+  before_action :find_service, except: %w(index create)
+
+  def index
+    @services = Service.where search_params
+    respond_with @services
+  end
+
+  def create
+    @service = Service.new create_params
+
+    if @service.save
+      respond_with @service
+    else
+      render json: { errors: @service.errors }
+    end
+  end
+
+  def show
+    respond_with @service
+  end
+
+  def update
+    if @service.update edit_params
+      respond_with @service
+    else
+      render json: { errors: @service.errors }
+    end
+  end
+
+  def destroy
+    if @service.destroy
+      respond_with @service
+    else
+      render json: { errors: @service.errors }
+    end
+  end
+
+  private
+  def find_service
+    @service = Service.find params[:id]
+  end
 end
