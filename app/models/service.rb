981af32dd4363b723a5300e2a3f067de1427f3a5
Tom Scott <tscott@telvue.com>
2014-06-14 11:53:23 -0400

    Add methods to ping the service for bg jobs

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,17 +1,15 @@
-
 # An application that sits in the TelVue co-lo space, which acts as a
 # service for multiple users and/or organizations. Services go down from
 # time to time, for various reasons, so they are kept in the database
 # and a status is read from them.
-
 class Service < ActiveRecord::Base
   has_many :reports
   accepts_nested_attributes_for :reports
 
-  before_validation :bootstrap_status_updates, :on => :create
-
   validates :name, presence: true
   validates :host, presence: true
+
+  after_create :ping!
 
   validate :host_is_valid_uri
 
@@ -20,14 +18,22 @@ class Service < ActiveRecord::Base
     reports.current
   end
 
+  # Queue this Service to be pinged at a later time.
+  def ping
+    PingService.enqueue self
+  end
+
+  # "Ping" this Service, right now, and record results in the status
+  # updates. This is called right after the Service model is initially
+  # created, to "bootstrap" the reports collection.
+  def ping!
+    self.reports << Report.for(self)
+  end
+
   private
   def host_is_valid_uri
     URI.parse host
   rescue StandardError => error
     errors.add :host, "is not valid URI: #{error.message}"
-  end
-
-  def bootstrap_status_updates
-    self.status_updates << StatusUpdate.for(self)
   end
 end
